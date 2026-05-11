a50e4cad64248a907bf982a16a0892adb5c846c3
Navjot Singh <118371053+navjot-nangia@users.noreply.github.com>
2026-05-11 18:17:37 -0500

    Add Express server

---
+++ server.js
@@ -0,0 +1,1 @@
+const express=require('express');const path=require('path');const app=express();const PORT=process.env.PORT||3000;app.use(express.static(path.join(__dirname,'public')));app.get('*',(_req,res)=>res.sendFile(path.join(__dirname,'public','index.html')));app.listen(PORT,()=>console.log(`Career profile website running on port ${PORT}`));
